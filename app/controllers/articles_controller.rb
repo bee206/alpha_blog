dd11ca073fb3985f452d1f24a5b2260c13ef8725
vietlt <vietltbee@gmail.com>
2018-11-24 01:00:41 +0700

    Edit some partials for project and add destroy action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -28,11 +28,17 @@ class ArticlesController < ApplicationController
 
   def update
     if @article.update(article_params)
-      flash[:notice] = "Article was successfully updated "
+      flash[:notice] = "Article was successfully updated"
       redirect_to article_path(@article)
     else
       render 'edit'
     end
+  end
+
+  def destroy
+    @article.destroy
+    flash[:notice] = "Article was successfully destroyed"
+    redirect_to articles_path
   end
 
   private
