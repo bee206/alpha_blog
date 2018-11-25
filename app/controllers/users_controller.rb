8e075b60ed79402a851c523063b34c856df6dc2c
vietlt <vietltbee@gmail.com>
2018-11-25 17:26:09 +0700

    Create show action and add gravatar image for user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :find_user, only: [:edit, :update]
+  before_action :find_user, only: [:edit, :update, :show]
 
   def new
     @user = User.new
